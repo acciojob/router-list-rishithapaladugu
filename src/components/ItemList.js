60e3703be469b1a45b153a756f8c571cb4403983
rishithapaladugu <116055808+rishithapaladugu@users.noreply.github.com>
2025-09-30 09:31:37 +0530

    Update ItemList.js

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -1,23 +1,23 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
-const items = [
-  { id: 1, name: "Item 1", description: "Description for Item 1" },
-  { id: 2, name: "Item 2", description: "Description for Item 2" },
-  { id: 3, name: "Item 3", description: "Description for Item 3" },
-];
-
 const ItemList = () => {
   return (
     <div>
       <h1>Item List</h1>
-      <ul>
-        {items.map((item) => (
-          <li key={item.id}>
-            <Link to={`/items/${item.id}`}>{item.name}</Link>
+
+      <nav>
+        <ul>
+          <li>
+            <Link to="/items/1">Item 1</Link>
           </li>
-        ))}
-      </ul>
+          <li>
+            <Link to="/items/2">Item 2</Link>
+          </li>
+          <li>
+            <Link to="/items/3">Item 3</Link>
+          </li>
+        </ul>
+      </nav>
     </div>
   );
 };
