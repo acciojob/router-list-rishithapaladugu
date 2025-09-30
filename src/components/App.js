ec11143b4795d93255789d8f939b08df8e98a588
rishithapaladugu <116055808+rishithapaladugu@users.noreply.github.com>
2025-09-30 09:30:07 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,18 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import './../styles/App.css';
 import ItemList from "./ItemList";
 import ItemDetail from "./ItemDetail";
+import {Routes, Route} from 'react-router-dom'
 
-function App() {
+const App = () => {
   return (
     <div>
-      <Routes>
-        <Route path="/" element={<ItemList />} />
-        <Route path="/items/:id" element={<ItemDetail />} />
-      </Routes>
+        <Routes>
+          <Route path='/' element={<ItemList />} />
+          <Route path='/items/:id' element={<ItemDetail />}/>
+        </Routes>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
